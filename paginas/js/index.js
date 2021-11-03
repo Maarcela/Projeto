d3562c5ee489c5b502d2f0d13fe25bc53f8d3868
JenniferPP <jenni-pp@hotmail.com>
2021-11-02 22:34:54 -0300

    Subindo arquivos no git

--- paginas/js/index.js
+++ paginas/js/index.js
@@ -1,4 +1,16 @@
-// INICIO MENU TOPO JS
+// INICIO MENU JS
+function abreMenu() {
+
+    var menu = document.getElementById('menu-nav');
+
+    if (menu.classList.contains('mostrar')) {
+        menu.classList.remove('mostrar');
+    } else {
+        menu.classList.add('mostrar');
+    }
+}
+
+// FIM MENU JS
 
 
 
