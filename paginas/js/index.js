333d755b317b5a87dac7e24d487f2f6d43d24c35
JenniferPP <jenni-pp@hotmail.com>
2021-11-04 20:52:24 -0300

    Menu parão em todas as páginas, já configurado e responsivo

--- paginas/js/index.js
+++ paginas/js/index.js
@@ -1,14 +1,4 @@
 // INICIO MENU JS
-function openNav() {
-    document.getElementById("navMenu").style.left = "0";
-    document.getElementById("navMenu").style.width = "250px";
-
-  }
-  
-  function closeNav() {
-    document.getElementById("navMenu").style.left = "-100%";
-    document.getElementById("navMenu").style.width = "0";
-  }
 
 // FIM MENU JS
 
