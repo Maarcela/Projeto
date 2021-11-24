b81a1c3aa9ee994a1f56dad873a4077c2aee5222
JenniferPP <jenni-pp@hotmail.com>
2021-11-24 15:57:22 -0300

    Ajustes e criação da página de contato

--- paginas/Contato/js/contato.js
+++ paginas/Contato/js/contato.js
@@ -63,22 +63,3 @@ $('.prodBtn').click(function(){
 $('nav ul li').click(function(){
     $(this).addClass("active").siblings().removeClass("active");
 });
-
-
-// Carousel de mais vendidos
-$(document).ready(function() {
-    $('#autoWidth').lightSlider({
-        autoWidth:true,
-        loop:true,
-        onSliderLoad: function() {
-            $('#autoWidth').removeClass('cS-hidden');
-        } 
-    });  
-  });
-
-
-//Validação do campo Newsletter
-console.clear()
-function validar(){
-    alert('Ficamos muito felizes por você fazer parte da nossa comunidade!')
-}   
